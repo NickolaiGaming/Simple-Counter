7b99ad57e52f4c634903cad5b4981aaf6b173c4d
Carlos Eduardo Tabare Camel <147793216+NickolaiGaming@users.noreply.github.com>
2024-01-17 17:43:25 +0000

    proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { useState , useEffect } from 'react';
+
+import SimpleCounter from "./component/SimpleCounter"
+
+
 
 const App = () => {
+    const [contador, setContador] = useState(0);
+
+
+    useEffect(()=>{
+        const interval = setInterval(() => {
+            setContador(contador => contador + 1)
+        }, 1000)
+        return () => clearInterval(interval)
+}, [contador])
+
+
+    function calcularSegundos(unContador, puesto){
+        return Math.floor(unContador / puesto) % 10
+    }
+
     return (
-        <h1>REACT APP</h1>
+        <>
+        <SimpleCounter 
+        miles={calcularSegundos(contador, 1000)}
+        centenas={calcularSegundos(contador, 100)}
+        decenas={calcularSegundos(contador, 10)}
+        segundos={calcularSegundos(contador, 1)}/>
+        </>
     )
 }
 
